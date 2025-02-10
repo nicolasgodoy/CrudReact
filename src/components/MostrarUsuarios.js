9a50715dc7e9bca4def100c41b82c6f8f2690491
nicolasgodoy <nicolasgodoy540@gmail.com>
2025-02-10 16:51:03 -0300

    subiendo url

--- src/components/MostrarUsuarios.js
+++ src/components/MostrarUsuarios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import {Link} from "react-router-dom";
 
 //const url = "https://localhost:44330/api/Usuario";
-const url = "http://dominioAngular.somee.com/api/Usuario"
+const url = "https://dominioAngular.somee.com/api/Usuario"
 
 // Terminar de estilizar y intentar subirlo a produccion para ponerlo en el portfolio 
 
